df3e30d3ebe870bb624d71667deb41b2c56081d7
Chiffle04 <95617821+Chiffle04@users.noreply.github.com>
2023-08-13 02:11:28 +0800

    Update and rename table-sort.js to jquery.dataTables.min.css

--- scripts/table-sort.js
+++
@@ -1,2 +0,0 @@
-<script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
-<link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
