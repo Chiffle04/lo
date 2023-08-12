843c59e5aa2248c5bc6969276ccf59fa50345052
Chiffle04 <95617821+Chiffle04@users.noreply.github.com>
2023-08-13 02:06:44 +0800

    Update table-sort.js

--- scripts/table-sort.js
+++ scripts/table-sort.js
@@ -1,4 +1,2 @@
-// Load the List.js library
-const list = new List('table-container', {
-  valueNames: ['header-1', 'header-2', 'header-3']
-});
+<script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
